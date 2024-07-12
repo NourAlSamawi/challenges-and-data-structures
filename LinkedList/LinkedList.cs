248d365ec948e573b94fe5c85fc991db15209096
Nour Alsamawi <20018788@student.ltuc.com>
2024-07-12 23:35:14 +0300

    linked list done

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace NewLinkedList
+{
+    internal class LinkedList
+    {
+        public Node head;
+
+        public void InsertToHead(Node current)
+        {
+            current.Next = head;
+            head = current;
+        }
+
+        public void InsertEnd(Node current)
+        {
+            if (head == null)
+            {
+                InsertToHead(current);
+                return;
+            }
+
+            Node node = head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            node.Next = current;
+        }
+
+        public bool Includes(int data)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data == data)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Remove(int data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("The list is empty.");
+                return;
+            }
+
+            if (head.Data == data)
+            {
+                head = head.Next;
+                return;
+            }
+
+            Node current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            if (head == null)
+            {
+                Console.WriteLine("Head -> Null");
+                return;
+            }
+
+            Console.Write("Head -> ");
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("Null");
+        }
+    }
+}
---
+++ LinkedList/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace NewLinkedList
+{
+    internal class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}

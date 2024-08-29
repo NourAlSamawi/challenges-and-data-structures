079495394a0fb7a15351fb1732dfa80c5881e315
Nour Alsamawi <20018788@student.ltuc.com>
2024-08-29 09:19:21 +0300

    stack and queue

---
+++ Data Structure/Stack&Queue/StackAndQueue/Stack&QueueTests/QueueTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackAndQueue;
+namespace Stack_QueueTests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void TestEnqueue()
+        {
+            var queue = new Queue();
+            queue.Enqueue(10);
+            Assert.Equal(10, queue.Peek());
+        }
+
+        [Fact]
+        public void TestDequeue()
+        {
+            var queue = new Queue();
+            queue.Enqueue(10);
+            int value = queue.Dequeue();
+            Assert.Equal(10, value);
+            Assert.True(queue.IsEmpty());
+        }
+
+        [Fact]
+        public void TestIsEmpty()
+        {
+            var queue = new Queue();
+            Assert.True(queue.IsEmpty());
+            queue.Enqueue(10);
+            Assert.False(queue.IsEmpty());
+        }
+    }
+
+}
---
+++ Data Structure/Stack&Queue/StackAndQueue/StackAndQueue/Queue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Queue
+    {
+        private Node front;
+        private Node rear;
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public void Enqueue(int data)
+        {
+            var newNode = new Node(data);
+            if (IsEmpty())
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = newNode;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty.");
+
+            int data = front.Data;
+            front = front.Next;
+            if (front == null)
+                rear = null;
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty.");
+
+            return front.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+
+        public void PrintQueue()
+        {
+            var current = front;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+}
